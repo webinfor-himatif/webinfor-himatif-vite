0887afba986732ea6d16a6be5dc62739611f88a5
UmamDev <umam.fdev@gmail.com>
2025-04-26 22:40:42 +0700

    update navbar + add responsive action

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,22 @@
 import './Nav.css';
 import horizontalLogo from '../img/HIMATIF Horizon.png';
-// import { useState } from 'react';
+import { useState } from 'react';
+import hamburger from "../img/icon/hamburger-menu.svg"
+import close from "../img/icon/close-menu.svg"
 
-// const [isActive, setActive] = useState(false);
-// const uel = document.getElementById('uel');
-// const open =  uel => uel.classList.toggle('open');
+
 const Nav = () => {
+    const [isShow, setShow] = useState(false);
+
+    function open(){
+        if(isShow){
+            setShow(false)
+        }else{
+            setShow(true)
+        }
+    }
+
+
     return ( 
         <>
             <nav>
@@ -16,8 +27,15 @@ const Nav = () => {
                     <li><a href="#divisi" className='navLink'>Divisi</a></li>
                     <li><a href="#footer" style={{padding: '7px 20px'}} className='navLink'>Contact</a></li>
                 </ul>
-                <button id='navToggler'> X </button>
+                <img src={isShow == true ? close : hamburger} id='navToggler' onClick={()=>open()}></img>
             </nav>
+            
+            <ul id='uel2' className={isShow == true ? "uel2 open" : "uel2 close"}>
+                <li><a href="#home" className='navLink'>Home</a></li>
+                <li><a href="#about" className='navLink'>About</a></li>
+                <li><a href="#divisi" className='navLink'>Divisi</a></li>
+                <li><a href="#footer" style={{padding: '7px 20px'}} className='navLink'>Contact</a></li>
+            </ul>
         </>      
      );
 }
